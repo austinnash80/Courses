81553da44b323c6d24892fa24d0f555ec653f301
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2018-05-07 00:02:12 +1000

    devise 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :datasheets do
     collection {post :import}
   end
+  root :to => "datasheets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
