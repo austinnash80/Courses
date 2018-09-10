eeadd86df41bb99b9617b6cf0b137798eb35fad3
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2018-09-10 10:49:58 -0700

    pdf upload

--- app/models/mailing.rb
+++ app/models/mailing.rb
@@ -5,6 +5,6 @@ class Mailing < ApplicationRecord
                       :path => "/:basename.:extension"
 
   # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :msi_art, :content_type => /\Aimage\/.*\Z/
-  # validates_attachment :msi_art, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
+  # validates_attachment_content_type :msi_art, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :msi_art, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
 end
