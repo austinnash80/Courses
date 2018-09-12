8383c21143ab642588a03a143831f2f6f23592d1
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2018-09-12 12:51:31 -0700

    batch

--- app/models/sequoia_customer.rb
+++ app/models/sequoia_customer.rb
@@ -6,11 +6,22 @@ class SequoiaCustomer < ApplicationRecord
   # validates :unique_id, :presence => true, :uniqueness => true
 
   def self.my_import(file)
-    sequoia_customers = []
+    batch,batch_size = [], 1_000
     CSV.foreach(file.path, headers: true, header_converters: :symbol, :encoding => 'utf-8') do |row|
-    sequoia_customers << SequoiaCustomer.new(row.to_hash)
+      batch << SequoiaCustomer.new(row.to_hash)
+      if batch.size >= batch_size
+        SequoiaCustomer.import batch
+          batch = []
     end
-    SequoiaCustomer.import sequoia_customers, recursive: true
   end
+  SequoiaCustomer.import batch
+  # def self.my_import(file)
+  #   sequoia_customers = []
+  #   CSV.foreach(file.path, headers: true, header_converters: :symbol, :encoding => 'utf-8') do |row|
+  #   sequoia_customers << SequoiaCustomer.new(row.to_hash)
+  #   end
+  #   SequoiaCustomer.import sequoia_customers, recursive: true
+  # end
 
+end
 end
