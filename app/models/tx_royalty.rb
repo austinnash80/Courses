fe51feaacdabb4d1e334d6d8de09e8177a79d98a
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2018-10-05 17:01:59 -0700

    tx3

--- app/models/tx_royalty.rb
+++ app/models/tx_royalty.rb
@@ -1,9 +1,9 @@
 class TxRoyalty < ApplicationRecord
 
-  has_attached_file :msi_art,
+  has_attached_file :sales_report,
                           :url =>':s3_domain_url',
                           :path => "/:basename.:extension"
 
-  validates_attachment :msi_art, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
+  validates_attachment :sales_report, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
 
 end
