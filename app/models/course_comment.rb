3d4dd881cb43619701b8d4c035a89821b4c31e05
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2018-12-28 11:19:05 -0800

    course commetns

--- app/models/course_comment.rb
+++ app/models/course_comment.rb
@@ -1,4 +1,5 @@
 class CourseComment < ApplicationRecord
   # self.primary_key = 'course_number'
   belongs_to :Datasheet, foreign_key: 'course_number'
+  validates :course_version, :presence => true
 end
