850be0ecd56d503d414f2763a370b2835b983c8b
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2020-01-28 19:26:29 -0800

    rc marketing

--- app/controllers/empire_customers_controller.rb
+++ app/controllers/empire_customers_controller.rb
@@ -31,8 +31,12 @@ class EmpireCustomersController < ApplicationController
     if params['page'] == 'deadline'
       rc_marketing_deadline
     end
+    if params['page'] == 'rolling' && params['what'] == 'email'
+      re_marketing_rolling_email
+    end
     rc_marketing_date
     rc_marketing_states
+    if params['what'] == 'postcard'
     @ca_master_1 = EmpireMasterList.where(source: 'CA').where(license_number: @ca_1).where.not(license_number: @ca_2).where('exp_date >= ? AND exp_date <= ?', @date_1a, @date_1b).pluck(:license_number)
     @ca_master_2 = EmpireMasterList.where(source: 'CA').where(license_number: @ca_1).where.not(license_number: @ca_2).where('exp_date >= ? AND exp_date <= ?', @date_2a, @date_2b).pluck(:license_number)
     @ny_master_1 = EmpireMasterList.where(source: 'NY').where(license_number: @ny_1).where.not(license_number: @ny_2).where('exp_date >= ? AND exp_date <= ?', @date_1a, @date_1b).pluck(:license_number)
@@ -208,9 +212,10 @@ class EmpireCustomersController < ApplicationController
           end
         end
 
-        redirect_to postcard_exports_path(co: 'empire', group: 'rc postcard', mail_id: "E-RC-Rolling-Postcard-#{params['day']}", day: params['day'], card: 'standard', sent: @exp_1 + @exp_2)
-
+        redirect_to postcard_exports_path(co: 'empire', group: 'rc postcard', mail_id: "E-RC-Rolling-Postcard-#{params['day']}", day: params['day'], card: 'postcard standard', sent: @exp_1 + @exp_2)
+      end
       end #If 'data'
+
   end
 
   def rc_marketing_date
@@ -241,6 +246,328 @@ class EmpireCustomersController < ApplicationController
       wa = EmpireCustomer.where(lic_state: 'WA').all #All NM Customers
       @wa_1 = wa.pluck(:license_num).uniq #Array of all unique license numbers
       @wa_2 = wa.where('p_date >= ?', Date.today - 18.months).pluck(:license_num).uniq #Array of unique license number who have purchases in the given time frame
+
+  end
+
+  def re_marketing_rolling_email
+
+## DAYS
+     day = params['day'].to_date
+      g1_day_a = ((day + 180.days).to_date).at_beginning_of_week
+      g1_day_b = ((day + 180.days).to_date).at_end_of_week
+      g2_day_a = ((day + 150.days).to_date).at_beginning_of_week
+      g2_day_b = ((day + 150.days).to_date).at_end_of_week
+      g3_day_a = ((day + 120.days).to_date).at_beginning_of_week
+      g3_day_b = ((day + 120.days).to_date).at_end_of_week
+      g4_day_a = ((day + 90.days).to_date).at_beginning_of_week
+      g4_day_b = ((day + 90.days).to_date).at_end_of_week
+      g5_day_a = ((day + 60.days).to_date).at_beginning_of_week
+      g5_day_b = ((day + 60.days).to_date).at_end_of_week
+      g6_day_a = ((day + 45.days).to_date).at_beginning_of_week
+      g6_day_b = ((day + 45.days).to_date).at_end_of_week
+      g7_day_a = ((day + 30.days).to_date).at_beginning_of_week
+      g7_day_b = ((day + 30.days).to_date).at_end_of_week
+      g8_day_a = ((day + 15.days).to_date).at_beginning_of_week
+      g8_day_b = ((day + 15.days).to_date).at_end_of_week
+      g9_day_a = ((day + 10.days).to_date).at_beginning_of_week
+      g9_day_b = ((day + 10.days).to_date).at_end_of_week
+      g10_day_a = ((day + 5.days).to_date).at_beginning_of_week
+      g10_day_b = ((day + 5.days).to_date).at_end_of_week
+      g11_day_a = ((day + 2.days).to_date).at_beginning_of_week
+      g11_day_b = ((day + 2.days).to_date).at_end_of_week
+## DAYS END
+
+## GROUP ARRAYS
+      g1_ca = []
+      g2_ca = []
+      g3_ca = []
+      g4_ca = []
+      g5_ca = []
+      g6_ca = []
+      g7_ca = []
+      g8_ca = []
+      g9_ca = []
+      g10_ca = []
+      g11_ca = []
+      g1_ny = []
+      g2_ny = []
+      g3_ny = []
+      g4_ny = []
+      g5_ny = []
+      g6_ny = []
+      g7_ny = []
+      g8_ny = []
+      g9_ny = []
+      g10_ny = []
+      g11_ny = []
+      g1_nm = []
+      g2_nm = []
+      g3_nm = []
+      g4_nm = []
+      g5_nm = []
+      g6_nm = []
+      g7_nm = []
+      g8_nm = []
+      g9_nm = []
+      g10_nm = []
+      g11_nm = []
+## GROUP ARRAYS END
+
+## Master Vaiables
+      ca_customer = EmpireCustomer.where(lic_state: 'CA').all
+      ny_customer = EmpireCustomer.where(lic_state: 'NY').all
+      nm_customer = EmpireCustomer.where(lic_state: 'NM').all
+      ca_master = EmpireMasterList.where(source: 'CA')
+      ny_master = EmpireMasterList.where(source: 'NY')
+      nm_master = EmpireMasterList.where(source: 'NM')
+## Master Vaiables END
+
+## GROUPS BY LICENSE NUMBER
+    g1_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g1_day_a, g1_day_b).pluck(:license_number)
+    g2_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g2_day_a, g2_day_b).pluck(:license_number)
+    g3_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g3_day_a, g3_day_b).pluck(:license_number)
+    g4_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g4_day_a, g4_day_b).pluck(:license_number)
+    g5_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g5_day_a, g5_day_b).pluck(:license_number)
+    g6_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g6_day_a, g6_day_b).pluck(:license_number)
+    g7_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g7_day_a, g7_day_b).pluck(:license_number)
+    g8_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g8_day_a, g8_day_b).pluck(:license_number)
+    g9_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g9_day_a, g9_day_b).pluck(:license_number)
+    g10_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g10_day_a, g10_day_b).pluck(:license_number)
+    g11_ca_master = ca_master.where('exp_date > ? AND exp_date <= ?', g11_day_a, g11_day_b).pluck(:license_number)
+    g1_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g1_day_a, g1_day_b).pluck(:license_number)
+    g2_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g2_day_a, g2_day_b).pluck(:license_number)
+    g3_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g3_day_a, g3_day_b).pluck(:license_number)
+    g4_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g4_day_a, g4_day_b).pluck(:license_number)
+    g5_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g5_day_a, g5_day_b).pluck(:license_number)
+    g6_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g6_day_a, g6_day_b).pluck(:license_number)
+    g7_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g7_day_a, g7_day_b).pluck(:license_number)
+    g8_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g8_day_a, g8_day_b).pluck(:license_number)
+    g9_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g9_day_a, g9_day_b).pluck(:license_number)
+    g10_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g10_day_a, g10_day_b).pluck(:license_number)
+    g11_ny_master = ny_master.where('exp_date > ? AND exp_date <= ?', g11_day_a, g11_day_b).pluck(:license_number)
+    g1_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g1_day_a, g1_day_b).pluck(:license_number)
+    g2_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g2_day_a, g2_day_b).pluck(:license_number)
+    g3_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g3_day_a, g3_day_b).pluck(:license_number)
+    g4_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g4_day_a, g4_day_b).pluck(:license_number)
+    g5_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g5_day_a, g5_day_b).pluck(:license_number)
+    g6_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g6_day_a, g6_day_b).pluck(:license_number)
+    g7_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g7_day_a, g7_day_b).pluck(:license_number)
+    g8_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g8_day_a, g8_day_b).pluck(:license_number)
+    g9_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g9_day_a, g9_day_b).pluck(:license_number)
+    g10_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g10_day_a, g10_day_b).pluck(:license_number)
+    g11_nm_master = nm_master.where('exp_date > ? AND exp_date <= ?', g11_day_a, g11_day_b).pluck(:license_number)
+## GROUPS END
+
+## EXCLUDES (custers who have already purchased for this renewal cycle)
+    ca_exclude = ca_customer.where('p_date > ?', Date.today - 2.year).pluck(:uid)
+    ny_exclude = ny_customer.where('p_date > ?', Date.today - 1.year).pluck(:uid)
+    nm_exclude = nm_customer.where('p_date > ?', Date.today - 1.year).pluck(:uid)
+## EXCLUDES END
+
+## ADDING TO GROUP ARRAY
+  #CA
+     ca_customer.each do |empire_customer|
+       if g1_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g1_ca.push(empire_customer.uid)
+       elsif g2_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g2_ca.push(empire_customer.uid)
+       elsif g3_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g3_ca.push(empire_customer.uid)
+       elsif g4_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g4_ca.push(empire_customer.uid)
+       elsif g5_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g5_ca.push(empire_customer.uid)
+       elsif g6_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g6_ca.push(empire_customer.uid)
+       elsif g7_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g7_ca.push(empire_customer.uid)
+       elsif g8_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g8_ca.push(empire_customer.uid)
+       elsif g9_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g9_ca.push(empire_customer.uid)
+       elsif g10_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g10_ca.push(empire_customer.uid)
+       elsif g11_ca_master.include?(empire_customer.license_num) && ca_exclude.exclude?(empire_customer.uid)
+         g11_ca.push(empire_customer.uid)
+       end
+     end
+  #NY
+   ny_customer.each do |empire_customer|
+     if g1_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g1_ny.push(empire_customer.uid)
+     elsif g2_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g2_ny.push(empire_customer.uid)
+     elsif g3_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g3_ny.push(empire_customer.uid)
+     elsif g4_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g4_ny.push(empire_customer.uid)
+     elsif g5_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g5_ny.push(empire_customer.uid)
+     elsif g6_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g6_ny.push(empire_customer.uid)
+     elsif g7_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g7_ny.push(empire_customer.uid)
+     elsif g8_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g8_ny.push(empire_customer.uid)
+     elsif g9_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g9_ny.push(empire_customer.uid)
+     elsif g10_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g10_ny.push(empire_customer.uid)
+     elsif g11_ny_master.include?(empire_customer.license_num) && ny_exclude.exclude?(empire_customer.uid)
+       g11_ny.push(empire_customer.uid)
+     end
+   end
+  #NM
+   nm_customer.each do |empire_customer|
+     if g1_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g1_nm.push(empire_customer.uid)
+     elsif g2_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g2_nm.push(empire_customer.uid)
+     elsif g3_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g3_nm.push(empire_customer.uid)
+     elsif g4_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g4_nm.push(empire_customer.uid)
+     elsif g5_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g5_nm.push(empire_customer.uid)
+     elsif g6_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g6_nm.push(empire_customer.uid)
+     elsif g7_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g7_nm.push(empire_customer.uid)
+     elsif g8_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g8_nm.push(empire_customer.uid)
+     elsif g9_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g9_nm.push(empire_customer.uid)
+     elsif g10_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g10_nm.push(empire_customer.uid)
+     elsif g11_nm_master.include?(empire_customer.license_num) && nm_exclude.exclude?(empire_customer.uid)
+       g11_nm.push(empire_customer.uid)
+     end
+   end
+ ## ADDING END
+
+## TOTALS
+groups_ca = [ g1_ca, g2_ca , g3_ca , g4_ca , g5_ca , g6_ca , g7_ca , g8_ca , g9_ca , g10_ca , g11_ca]
+groups_ny = [ g1_ny, g2_ny , g3_ny , g4_ny , g5_ny , g6_ny , g7_ny , g8_ny , g9_ny , g10_ny , g11_ny]
+groups_nm = [ g1_nm, g2_nm , g3_nm , g4_nm , g5_nm , g6_nm , g7_nm , g8_nm , g9_nm , g10_nm , g11_nm]
+@ca_count = []
+ca_uid = []
+@ny_count = []
+ny_uid = []
+@nm_count = []
+nm_uid = []
+groups_ca.each do |i| @ca_count.push(i.uniq.count)
+  i.each do |uid|
+    ca_uid.push(uid)
+  end
+end
+groups_ny.each do |i| @ny_count.push(i.uniq.count)
+  i.each do |uid|
+    ny_uid.push(uid)
+  end
+end
+groups_nm.each do |i| @nm_count.push(i.uniq.count)
+  i.each do |uid|
+    nm_uid.push(uid)
+  end
+end
+
+
+# #ADD
+  if params['added'] == 'done'
+
+    PostcardExport.delete_all #Delete all the current records - fresh database each time.
+
+    added_ca_uid = []
+    ca_customer.each do |empire_customer|
+      if ca_uid.include? empire_customer.uid
+        unless added_ca_uid.include? empire_customer.uid
+          new = PostcardExport.create(
+            company: 'Empire',
+            group: 'rc email',
+            mail_id: "E-RC-Rolling-Email-#{params['day']}",
+            mail_date: params['day'],
+            state: 'CA',
+            license_number: empire_customer.license_num,
+            uid: empire_customer.uid,
+            merge_1: 'Merge Text 1',
+            merge_2: 'Merge Text 1',
+            merge_3: 'Merge Text 1',
+            f_name: empire_customer.fname,
+            l_name: empire_customer.lname,
+            add_1: empire_customer.street_1,
+            add_2: empire_customer.street_2,
+            city: empire_customer.city,
+            st: empire_customer.state,
+            zip: empire_customer.zip,
+            email: empire_customer.email)
+          new.save
+          added_ca_uid.push(empire_customer.uid) #Push new records lic number in to array to not allow duplicates in new table
+        end
+      end
+    end
+    added_ny_uid = []
+    ny_customer.each do |empire_customer|
+      if ny_uid.include? empire_customer.uid
+        unless added_ny_uid.include? empire_customer.uid
+          new = PostcardExport.create(
+            company: 'Empire',
+            group: 'rc email',
+            mail_id: "E-RC-Rolling-Email-#{params['day']}",
+            mail_date: params['day'],
+            state: 'NY',
+            license_number: empire_customer.license_num,
+            uid: empire_customer.uid,
+            merge_1: 'Merge Text 1',
+            merge_2: 'Merge Text 1',
+            merge_3: 'Merge Text 1',
+            f_name: empire_customer.fname,
+            l_name: empire_customer.lname,
+            add_1: empire_customer.street_1,
+            add_2: empire_customer.street_2,
+            city: empire_customer.city,
+            st: empire_customer.state,
+            zip: empire_customer.zip,
+            email: empire_customer.email)
+          new.save
+          added_ny_uid.push(empire_customer.uid) #Push new records lic number in to array to not allow duplicates in new table
+        end
+      end
+    end
+    added_nm_uid = []
+    nm_customer.each do |empire_customer|
+      if nm_uid.include? empire_customer.uid
+        unless added_nm_uid.include? empire_customer.uid
+          new = PostcardExport.create(
+            company: 'Empire',
+            group: 'rc email',
+            mail_id: "E-RC-Rolling-Email-#{params['day']}",
+            mail_date: params['day'],
+            state: 'NM',
+            license_number: empire_customer.license_num,
+            uid: empire_customer.uid,
+            merge_1: 'Merge Text 1',
+            merge_2: 'Merge Text 1',
+            merge_3: 'Merge Text 1',
+            f_name: empire_customer.fname,
+            l_name: empire_customer.lname,
+            add_1: empire_customer.street_1,
+            add_2: empire_customer.street_2,
+            city: empire_customer.city,
+            st: empire_customer.state,
+            zip: empire_customer.zip,
+            email: empire_customer.email)
+          new.save
+          added_nm_uid.push(empire_customer.uid) #Push new records lic number in to array to not allow duplicates in new table
+        end
+      end
+    end
+
+    redirect_to postcard_exports_path(co: 'empire', group: 'rc email', mail_id: "E-RC-Rolling-Email-#{params['day']}", day: params['day'], what: params['what'], card: 'email', sent: @ca_count.sum+@ny_count.sum+@nm_count.sum)
+
+  end
+
+
+## TOTALS END
 
   end
 
