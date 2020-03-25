dfcf323f7caf9388c2c70f545c56cf6cd419faa0
Austin Nash <austinnash@austins-mbp.attlocal.net>
2020-03-25 10:04:18 -0700

    email 2

--- app/models/email_export.rb
+++ app/models/email_export.rb
@@ -1,4 +1,8 @@
 class EmailExport < ApplicationRecord
+
+  validates :email, presence: true
+  validates_uniqueness_of :email
+
   def self.to_csv # Export to csv function
     attributes = %w{send_email company subject merge_1 merge_2 merge_3 merge_4 merge_5 f_name l_name email}
     CSV.generate(headers: true) do |csv|
