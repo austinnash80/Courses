fa5fdc0e193fa7eb69ec2f6ba497cbc20e75ae6c
Austin Nash <austinnash@austins-mbp.attlocal.net>
2021-01-26 02:14:20 -0800

    update

--- db/migrate/20210126095952_add_list_to_master_cpa_double_account.rb
+++ db/migrate/20210126095952_add_list_to_master_cpa_double_account.rb
@@ -1,6 +1,5 @@
 class AddListToMasterCpaDoubleAccount < ActiveRecord::Migration[5.1]
   def change
     add_column :master_cpa_double_accounts, :list, :string
-    add_column :master_cpa_double_accounts, :lid, :integer
   end
 end
