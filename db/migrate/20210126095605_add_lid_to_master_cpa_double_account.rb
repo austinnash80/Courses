28297f48193c51d4b53cffee548012c2922f6e4e
Austin Nash <austinnash@austins-mbp.attlocal.net>
2021-01-26 02:08:40 -0800

    update

--- db/migrate/20210126095605_add_lid_to_master_cpa_double_account.rb
+++
@@ -1,6 +0,0 @@
-class AddLidToMasterCpaDoubleAccount < ActiveRecord::Migration[5.1]
-  def change
-    add_column :master_cpa_double_accounts, :lid, :string
-    add_column :master_cpa_double_accounts, :integer, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210126095605) do
+ActiveRecord::Schema.define(version: 20210126095952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -549,8 +549,8 @@ ActiveRecord::Schema.define(version: 20210126095605) do
     t.date "search_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "lid"
-    t.string "integer"
+    t.string "list"
+    t.integer "lid"
   end
 
   create_table "master_cpa_matches", force: :cascade do |t|
